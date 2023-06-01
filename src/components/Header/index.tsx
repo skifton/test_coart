d9669948083f73f9f979b4372d02a1e2ec2e0664
Anton Simanenkau <antoniestories@gmail.com>
2023-06-01 11:52:03 +0400

    fixed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,17 +9,19 @@ const Header: React.FC = () => {
 
   return (
     <header className="py-12">
-      <LayoutWrapper className="flex items-center">
-        <img
-          className="w-20 h-20 mr-8"
-          src={CoArtLogo}
-          alt={intl.formatMessage({ id: "LOGO" })}
-        />
-        <h1 className="text-3xl text-white font-semibold tracking-widest">
-          <FormattedMessage id="COART_PLATFORM" />
-        </h1>
+      <LayoutWrapper className="flex items-center justify-between flex-col md:flex-row">
+        <div className="flex items-center">
+          <img
+            className="w-20 h-20 mr-8"
+            src={CoArtLogo}
+            alt={intl.formatMessage({ id: "LOGO" })}
+          />
+          <h1 className="text-3xl text-white font-semibold tracking-widest">
+            <FormattedMessage id="COART_PLATFORM" />
+          </h1>
+        </div>
 
-        <Button>DATA ROOM</Button>
+        <Button className="px-3 w-full mt-10 sm:w-max md:mt-0">DATA ROOM</Button>
       </LayoutWrapper>
     </header>
   );
